447c3954a8bd33959d65b2245654a6ede7ffb617
unknown <895470730@qq.com>
2019-04-08 21:46:28 +0800

    完善记一笔帐的功能

--- src/component/RecordBox.js
+++ src/component/RecordBox.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import {Modal} from "antd";
+import {Modal, Form, Input, Cascader} from "antd";
 import store from '../store/index';
 import {getChangeRecordBoxShow} from '../store/actionCreator';
+const options = [{
+	value: 'zhejiang',
+	label: 'Zhejiang',
+	children: [{
+		value: 'hangzhou',
+		label: 'Hangzhou',
+		children: [{
+			value: 'xihu',
+			label: 'West Lake',
+		}],
+	}],
+}]
+function handleAreaClick(e, label, option) {
+	e.stopPropagation();
+	console.log('clicked', label, option);
+}
+
+const displayRender = (labels, selectedOptions) => labels.map((label, i) => {
+	const option = selectedOptions[i];
+	if (i === labels.length - 1) {
+		return (
+			<span key={option.value}>
+        {label} (<a onClick={e => handleAreaClick(e, label, option)}>{option.code}</a>)
+      </span>
+		);
+	}
+	return <span key={option.value}>{label} / </span>;
+});
 class RecordBox extends React.Component{
 	constructor(props){
 		super(props);
@@ -13,14 +41,31 @@ class RecordBox extends React.Component{
 		const action = getChangeRecordBoxShow();
 		store.dispatch(action);
 	};
+	handleOk = () =>{
+
+	};
 	render() {
 		return(
 			<Modal
 				visible={this.state.record_box_show}
+				title="Create a new collection"
+				okText="Create"
 				onCancel={this.handleCancel}
+				onOk={this.handleOk}
 			>
-
-			</Modal>
+					<Form layout="vertical">
+						<Form.Item>
+							花销类别：
+							<InputGroup compact>
+								<Select defaultValue="Zhejiang">
+									<Option value="Zhejiang">Zhejiang</Option>
+									<Option value="Jiangsu">Jiangsu</Option>
+								</Select>
+								<Input style={{ width: '50%' }} defaultValue="Xihu District, Hangzhou" />
+							</InputGroup>
+						</Form.Item>
+					</Form>
+				</Modal>
 		)
 	}
 }
